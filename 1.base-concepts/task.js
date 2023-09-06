f0a103ade6efb09d8cff42a2d36bf104421d9500
Mikhail Kodintsev <100272818+newalexracer@users.noreply.github.com>
2023-09-06 19:37:38 +0300

    Update task.js

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,10 +1,32 @@
-"use strict"
-function solveEquation(a, b, c) {
-  let arr = [];
-  
-  return arr;
+"use strict"; // Входим в строгий режим
+// ЗАДАЧА № 1
+function solveEquation() {
+  let a = +window.a.value;
+  let b = +window.b.value;
+  let c = +window.c.value;
+  let result = getResult(a, b, c);
+  window.equation.textContent = `${a}*x^2 + (${b})*x + (${c}) = 0`;
+  let span = window.result;
+  span.textContent = "х = " + result;
 }
+// Реализация функцию getResult(a,b,c)
+function getResult(a, b, c) {
+  let result = "";
+  let discrim = b ** 2 - 4 * a * c; // Вычисляем дискриминант
+  // Находим корень квадратного уровнения в зависимости от условия
+  if (discrim < 0) {
+    result = null;
+    console.log(result);
+  } else if (discrim == 0) {
+    result = (-b + Math.sqrt(discrim)) / (2 * a);
+    console.log(result);
+  } else if (discrim > 0) {
+    result = [
+      (-b + Math.sqrt(discrim)) / (2 * a),
+      (-b - Math.sqrt(discrim)) / (2 * a),
+    ];
+    console.log(result);
+  }
 
-function calculateTotalMortgage(percent, contribution, amount, countMonths) {
-  
+  return result;
 }
